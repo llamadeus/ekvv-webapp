6b7e6ba41ad14335957803b63101e01d188248eb
Amadeus Beckmann <amadeus@beckmann.io>
2019-03-25 01:46:38 +0100

    Add express server

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const setupProxy = require('./setupProxy');
+
+
+const app = express();
+const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('sslcert/server.cert', 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+setupProxy(app);
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+https.createServer(credentials, app).listen(3000, function () {
+  console.log('eKVV Proxy server listening on port 3000!');
+});
