1145f9b014ac8caff444b6387c0b0ee772092d2f
Amadeus Beckmann <amadeus@beckmann.io>
2019-03-31 21:42:23 +0200

    Print info after hot reloading reducers or sagas

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -45,6 +45,7 @@ export default function configureStore(history) {
     if (module.hot) {
       module.hot.accept('../reducers', () => {
         store.replaceReducer(createRootReducer(history));
+        console.log('[HMR] Reloaded reducer'); // eslint-disable-line no-console
       });
 
       module.hot.accept('../sagas', () => {
@@ -53,6 +54,7 @@ export default function configureStore(history) {
         }
 
         sagaTask = sagaMiddleware.run(rootSaga);
+        console.log('[HMR] Reloaded saga'); // eslint-disable-line no-console
       });
     }
   }
