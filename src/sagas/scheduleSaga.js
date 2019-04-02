339df7ca1b01fc5ecccf2ec8caa6552a59b43518
Amadeus Beckmann <amadeus@beckmann.io>
2019-04-02 09:44:21 +0200

    Show warning when calendar is empty or invalid

--- src/sagas/scheduleSaga.js
+++ src/sagas/scheduleSaga.js
@@ -1,4 +1,5 @@
 import ical2json from 'ical2json';
+import { Modal } from 'antd';
 import moment from 'moment';
 import {
   all,
@@ -15,6 +16,45 @@ import { loadEvents } from './database';
 
 
 /**
+ * Load the calendar
+ *
+ * @param url
+ * @param text
+ * @returns {IterableIterator<*>}
+ */
+function* loadCalendar(url, text) {
+  const calendar = ical2json.convert(text);
+  const vCalendar = calendar.VCALENDAR;
+  const vEvents = Array.isArray(vCalendar) && vCalendar.length === 1
+    ? (vCalendar[0].VEVENT || [])
+    : [];
+
+  if (vEvents.length === 0) {
+    Modal.warning({
+      title: 'Dein Stundenplan ist leer.',
+      content: 'Du hast dich entweder für keine Kurse angemeldet oder die URL zu deinem persönlichen Kalendar ist falsch.',
+      okText: 'Ok und abbrechen',
+    });
+
+    return false;
+  }
+
+  yield database.events.bulkPut(vEvents.map(event => ({
+    uid: event.UID,
+    start: moment(event['DTSTART;TZID=Europe/Berlin']).toDate(),
+    end: moment(event['DTEND;TZID=Europe/Berlin']).toDate(),
+    description: event.DESCRIPTION,
+    location: event.LOCATION,
+    rrule: event.RRULE,
+    summary: event.SUMMARY,
+    url: event.URL,
+    raw: event,
+  })));
+
+  return true;
+}
+
+/**
  * Load the schedule.
  *
  * @param payload
@@ -27,28 +67,14 @@ function* handleLoadSchedule({ payload }) {
     const url = payload.url.replace('https://ekvv.uni-bielefeld.de', '/api');
     const request = yield call(fetch, url);
     const text = yield request.text();
-    const calendar = ical2json.convert(text);
-    const vCalendar = calendar.VCALENDAR;
-    const vEvents = Array.isArray(vCalendar) && vCalendar.length === 1
-      ? (vCalendar[0].VEVENT || [])
-      : [];
+    const success = yield call(loadCalendar, payload.url, text);
 
-    yield keyval.set(KEYS.ICAL_URL, payload.url);
-    yield keyval.set(KEYS.ICAL_RAW, text);
+    if (success) {
+      yield keyval.set(KEYS.ICAL_URL, url);
+      yield keyval.set(KEYS.ICAL_RAW, text);
 
-    yield database.events.bulkPut(vEvents.map(event => ({
-      uid: event.UID,
-      start: moment(event['DTSTART;TZID=Europe/Berlin']).toDate(),
-      end: moment(event['DTEND;TZID=Europe/Berlin']).toDate(),
-      description: event.DESCRIPTION,
-      location: event.LOCATION,
-      rrule: event.RRULE,
-      summary: event.SUMMARY,
-      url: event.URL,
-      raw: event,
-    })));
-
-    yield call(loadEvents);
+      yield call(loadEvents);
+    }
   }
   finally {
     yield put(setLoadingState(false));
