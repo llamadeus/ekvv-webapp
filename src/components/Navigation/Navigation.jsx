f1ccc0002e8e3481758e3c9fef7e7b4e55019c8e
Amadeus Beckmann <amadeus@beckmann.io>
2019-04-02 02:28:21 +0200

    Increase navigation brand size

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { Layout } from 'antd';
 import React from 'react';
 import { isWebapp } from '../../utils/app';
+import styles from './styles.module.scss';
 
 
 /**
@@ -20,7 +21,7 @@ export default class Navigation extends React.PureComponent {
     return (
       <Layout.Header className="tw-text-white">
         <div className="tw-max-w-sm tw-mx-auto xs:tw-px-4">
-          eKVV
+          <span className={styles.brand}>eKVV</span>
         </div>
       </Layout.Header>
     );
