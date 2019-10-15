020cafd9419b12a1d0ff3a5513561ef6974919a5
Amadeus Beckmann <amadeus@beckmann.io>
2019-10-16 00:59:52 +0200

    Switch to media query based approach to detect standalone mode

--- src/utils/app.js
+++ src/utils/app.js
@@ -4,9 +4,7 @@
  * @returns {boolean}
  */
 export function isWebapp() {
-  const params = new URLSearchParams(document.location.search.substring(1));
-
-  return params.get('webapp') === '1';
+  return window.matchMedia('(display-mode: standalone)').matches;
 }
 
 /**
