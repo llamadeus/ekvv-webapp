97f63dde8cee807a986d13833234aae1749f072e
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-05 13:24:01 +0100

    Convert LoadingSpinner class component to functional component

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -6,19 +6,14 @@ import React from 'react';
 
 
 /**
- * Class LoadingSpinner
+ * LoadingSpinner component
+ *
+ * @returns {*}
  */
-export default class LoadingSpinner extends React.PureComponent {
-  /**
-   * Render the component.
-   *
-   * @return {*}
-   */
-  render() {
-    return (
-      <div className="tw-flex tw-flex-1 tw-items-center tw-justify-center">
-        <Spin indicator={<Icon type="loading" style={{ fontSize: 60 }} spin/>}/>
-      </div>
-    );
-  }
+export default function LoadingSpinner() {
+  return (
+    <div className="tw-flex tw-flex-1 tw-items-center tw-justify-center">
+      <Spin indicator={<Icon type="loading" style={{ fontSize: 60 }} spin/>}/>
+    </div>
+  );
 }
