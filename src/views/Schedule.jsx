65e0eda719d352369da702fce49bcb62598f50bf
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-18 01:06:48 +0100

    Convert Schedule class component to functional component

--- src/views/Schedule.jsx
+++ src/views/Schedule.jsx
@@ -3,71 +3,38 @@ import {
   setSelectedDay,
 } from 'app/actions/schedule';
 import WeekScroller from 'app/components/WeekScroller';
-import { Moment } from 'app/prop-types';
 import {
   getRequestedDay,
   getSelectedDay,
   getSelectedWeek,
 } from 'app/selectors/schedule';
-import { DayShape } from 'app/shapes/schedule';
+import React, { useCallback } from 'react';
 import {
-  mapDispatchToProps,
-  mapStateToProps,
-} from 'app/utils/redux';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { bindActionCreators } from 'redux';
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 
 
 /**
- * Class Schedule
+ * Schedule component
+ *
+ * @returns {*}
  */
-@mapStateToProps(state => ({
-  selectedWeek: getSelectedWeek(state),
-  selectedDay: getSelectedDay(state),
-  requestedDay: getRequestedDay(state),
-}))
-@mapDispatchToProps(dispatch => bindActionCreators({
-  onSetSelectedDay: setSelectedDay,
-  onResetRequestedDay: resetRequestedDay,
-}, dispatch))
-export default class Schedule extends React.PureComponent {
-  /**
-   * Prop types.
-   *
-   * @type {Object}
-   */
-  static propTypes = {
-    selectedWeek: Moment.isRequired,
-    selectedDay: DayShape.isRequired,
-    requestedDay: DayShape,
-    onSetSelectedDay: PropTypes.func.isRequired,
-    onResetRequestedDay: PropTypes.func.isRequired,
-  };
+export default function Schedule() {
+  const selectedWeek = useSelector(getSelectedWeek);
+  const selectedDay = useSelector(getSelectedDay);
+  const requestedDay = useSelector(getRequestedDay);
+  const dispatch = useDispatch();
+  const handleSetSelectedDay = useCallback(day => dispatch(setSelectedDay(day)), [dispatch]);
+  const handleResetRequestedDay = useCallback(() => dispatch(resetRequestedDay()), [dispatch]);
 
-  /**
-   * Default props.
-   *
-   * @type {Object}
-   */
-  static defaultProps = {
-    requestedDay: null,
-  };
-
-  /**
-   * Render the component.
-   *
-   * @return {*}
-   */
-  render() {
-    return (
-      <WeekScroller
-        selectedWeek={this.props.selectedWeek}
-        selectedDay={this.props.selectedDay}
-        requestedDay={this.props.requestedDay}
-        onSetSelectedDay={this.props.onSetSelectedDay}
-        onResetRequestedDay={this.props.onResetRequestedDay}
-      />
-    );
-  }
+  return (
+    <WeekScroller
+      selectedWeek={selectedWeek}
+      selectedDay={selectedDay}
+      requestedDay={requestedDay}
+      onSetSelectedDay={handleSetSelectedDay}
+      onResetRequestedDay={handleResetRequestedDay}
+    />
+  );
 }
