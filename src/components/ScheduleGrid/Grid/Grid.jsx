56ebae43bf4762c009aa0faf90aed5e8f2ba36c3
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-18 02:11:47 +0100

    Convert Grid class component to functional component

--- src/components/ScheduleGrid/Grid/Grid.jsx
+++ src/components/ScheduleGrid/Grid/Grid.jsx
@@ -2,69 +2,46 @@ import Line from 'app/components/ScheduleGrid/Line';
 import TimeIndicator from 'app/components/TimeIndicator';
 import { range } from 'lodash-es';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './styles.module.scss';
 
 
 /**
- * Class Grid
+ * Grid component
+ *
+ * @param props
+ * @returns {*}
  */
-export default class Grid extends React.PureComponent {
-  /**
-   * Prop types.
-   *
-   * @type {Object}
-   */
-  static propTypes = {
-    start: PropTypes.number.isRequired,
-    end: PropTypes.number.isRequired,
-    showTimeIndicator: PropTypes.bool.isRequired,
-  };
-
-  /**
-   * Render the component.
-   *
-   * @return {*}
-   */
-  render() {
-    return (
-      <div className={styles.root}>
-        {this.renderTimeBlocks()}
-        {this.maybeRenderTimeIndicator()}
-      </div>
-    );
-  }
-
-  /**
-   * Render all time blocks.
-   *
-   * @returns {*}
-   */
-  renderTimeBlocks() {
-    const { start, end } = this.props;
-
-    return range(end - start + 1).map(time => (
-      <div key={time} className={styles.item}>
-        <Line label={`${(time + start).toString().padStart(2, '0')}:00`}/>
-      </div>
-    ));
-  }
-
-  /**
-   * Render the time indicator if enabled.
-   *
-   * @returns {boolean|*}
-   */
-  maybeRenderTimeIndicator() {
-    if (!this.props.showTimeIndicator) {
+export default function Grid(props) {
+  const { start, end, showTimeIndicator } = props;
+  const timeBlocks = useMemo(() => range(end - start + 1).map(time => (
+    <div key={time} className={styles.item}>
+      <Line label={`${(time + start).toString().padStart(2, '0')}:00`}/>
+    </div>
+  )), [start, end]);
+  const maybeTimeIndicator = useMemo(() => {
+    if (!showTimeIndicator) {
       return false;
     }
 
     return (
       <TimeIndicator
-        start={this.props.start}
-        end={this.props.end}
+        start={start}
+        end={end}
       />
     );
-  }
+  }, [showTimeIndicator, start, end]);
+
+  return (
+    <div className={styles.root}>
+      {timeBlocks}
+      {maybeTimeIndicator}
+    </div>
+  );
 }
+
+Grid.propTypes = {
+  start: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  showTimeIndicator: PropTypes.bool.isRequired,
+};
