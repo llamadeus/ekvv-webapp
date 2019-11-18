9b06ab5fc80fd1e47a46f304c33359094d0bbbfe
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-18 02:06:01 +0100

    Convert Start class component to functional component

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -6,88 +6,68 @@ import {
 } from 'antd';
 import { loadCalendar } from 'app/effects/schedule';
 import { getIsLoading } from 'app/selectors/ui';
-import {
-  mapDispatchToProps,
-  mapStateToProps,
-} from 'app/utils/redux';
-import PropTypes from 'prop-types';
 import { formShape } from 'rc-form';
-import React from 'react';
-import { bindActionCreators } from 'redux';
+import React, { useCallback } from 'react';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 
 
 /**
- * Class Start
+ * Start component
+ *
+ * @param props
+ * @returns {*}
  */
-@mapStateToProps(state => ({
-  isLoading: getIsLoading(state),
-}))
-@mapDispatchToProps(dispatch => bindActionCreators({
-  onLoadCalendar: loadCalendar,
-}, dispatch))
-@Form.create()
-export default class Start extends React.PureComponent {
-  /**
-   * Prop types.
-   *
-   * @type {Object}
-   */
-  static propTypes = {
-    form: formShape.isRequired,
-    isLoading: PropTypes.bool.isRequired,
-    onLoadCalendar: PropTypes.func.isRequired,
-  };
-
-  /**
-   * Handle form submission.
-   *
-   * @param event
-   */
-  handleSubmit = (event) => {
+function Start(props) {
+  const { getFieldDecorator, validateFields } = props.form;
+  const isLoading = useSelector(getIsLoading);
+  const dispatch = useDispatch();
+  const handleSubmit = useCallback((event) => {
     event.preventDefault();
 
-    this.props.form.validateFields((error, values) => {
+    validateFields((error, values) => {
       if (!error) {
-        this.props.onLoadCalendar(values.url);
+        dispatch(loadCalendar(values.url));
       }
     });
-  };
+  }, [validateFields, dispatch]);
 
-  /**
-   * Render the component.
-   *
-   * @return {*}
-   */
-  render() {
-    const { getFieldDecorator } = this.props.form;
+  return (
+    <Card title="Stundenplan laden">
+      <p>Füge die URL zu deinem persönlichen Kalender ein, um deinen Stundenplan zu laden.</p>
 
-    return (
-      <Card title="Stundenplan laden">
-        <p>Füge die URL zu deinem persönlichen Kalender ein, um deinen Stundenplan zu laden.</p>
-
-        <Form onSubmit={this.handleSubmit} hideRequiredMark>
-          <Form.Item>
-            {getFieldDecorator('url', {
-              validateTrigger: false,
-              rules: [{
+      <Form onSubmit={handleSubmit} hideRequiredMark>
+        <Form.Item>
+          {getFieldDecorator('url', {
+            validateTrigger: false,
+            rules: [
+              {
                 required: true,
                 message: 'Ohne geht nicht...',
               }, {
                 pattern: /^https:\/\/ekvv\.uni-bielefeld\.de\/ws\/calendar\?token=[a-zA-Z0-9]+$/,
                 message: 'Ungültiges Format',
-              }],
-            })(
-              <Input placeholder="https://ekvv.uni-bielefeld.de/ws/calendar?token=XYZ"/>,
-            )}
-          </Form.Item>
+              },
+            ],
+          })(
+            <Input placeholder="https://ekvv.uni-bielefeld.de/ws/calendar?token=XYZ"/>,
+          )}
+        </Form.Item>
 
-          <Form.Item className="tw-text-right">
-            <Button type="primary" htmlType="submit" loading={this.props.isLoading}>
-              Los!
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    );
-  }
+        <Form.Item className="tw-text-right">
+          <Button type="primary" htmlType="submit" loading={isLoading}>
+            Los!
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
+
+Start.propTypes = {
+  form: formShape.isRequired,
+};
+
+export default Form.create()(Start);
