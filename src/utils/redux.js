1fb86c678cdb03e92f446a23ec9651e770ab95f0
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-26 20:03:12 +0100

    Remove unused code

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -2,26 +2,7 @@ import {
   fromJS,
   Map,
 } from 'immutable';
-import { connect } from 'react-redux';
 
-
-/**
- * Map state to props.
- *
- * @param stateToProps
- */
-export function mapStateToProps(stateToProps) {
-  return connect(stateToProps);
-}
-
-/**
- * Map dispatch to props.
- *
- * @param dispatchToProps
- */
-export function mapDispatchToProps(dispatchToProps) {
-  return connect(undefined, dispatchToProps);
-}
 
 /**
  * Scope the given types by prefixing each key with `scope`.
--- src/utils/time.js
+++ src/utils/time.js
@@ -8,13 +8,3 @@
 export function timeToMinutes(hours, minutes) {
   return hours * 60 + minutes;
 }
-
-/**
- * Compute the time in minutes from the given moment instance.
- *
- * @param moment
- * @returns {*}
- */
-export function momentToMinutes(moment) {
-  return timeToMinutes(moment.hours(), moment.minutes());
-}
