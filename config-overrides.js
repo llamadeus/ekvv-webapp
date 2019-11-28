063ba17bf05721f1bfce6b60cd759cc79e972b51
Amadeus Beckmann <amadeus@beckmann.io>
2019-11-28 14:39:49 +0100

    Remove decorator support

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,5 @@
 const {
   override,
-  addDecoratorsLegacy,
   addWebpackAlias,
   adjustWorkbox,
   disableEsLint,
@@ -9,7 +8,6 @@ const webpackConfig = require('./webpack/webpack.config');
 
 
 module.exports = override(
-  addDecoratorsLegacy(),
   addWebpackAlias(webpackConfig.resolve.alias),
   disableEsLint(),
   adjustWorkbox(wb => Object.assign(wb, {
